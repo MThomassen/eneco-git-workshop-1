cbf1eebefe717d142902d0fac6e1d0c8794b953c
Michiel Thomassen <Michiel.Thomassen@eneco.com>
2020-07-20 08:59:36 +0200

    Trying to fix 'index of of range' error

--- lib/logic.py
+++ lib/logic.py
@@ -4,4 +4,10 @@ import sys
 def name_from_cli():
     """The invokers name is the first argument passed on the cli"""
 
+    # Index out of rage error, try:
+    # if len(sys.argv) > 1:
+    #     then return sys.argv[1]
+    # else:
+    #     return "Some default value"
+
     return sys.argv[1]
